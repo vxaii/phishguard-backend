ddc887113ab3574a917e760cba0e618609e627f0
VikiAinunnikmah <vikiainunnikmah273@gmail.com>
2026-08-13 20:55:22 +0700

    feat: connect backend to Supabase PostgreSQL cloud database

--- database.py
+++ database.py
@@ -1,12 +1,24 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
+DEFAULT_SUPABASE_URL = "postgresql://postgres.godhpkqjtccxspmsnqcs:Ainunviki273@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL", DEFAULT_SUPABASE_URL)
+
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+if "sqlite" in SQLALCHEMY_DATABASE_URL:
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, pool_pre_ping=True
+    )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
